e1847546e0224c9c86f99300a6fdd402f92e05fd
tmoffat <tom.moffat@viasat.com>
2018-08-09 08:56:03 +0100

    Forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/tsmoffat/clirescue/cmdutil"
+	"github.com/tsmoffat/clirescue/user"
 )
 
 var (
